4324aef0c79d6d1e05932e702999fd174daf8173
ONEO <oneo.me@outlook.com>
2020-03-29 22:58:05 +0800

    优化示例

--- Arthas.Demo/MainWindow_Model.cs
+++ Arthas.Demo/MainWindow_Model.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading.Tasks;
-using Arthas.Binder;
+﻿using Arthas.Binder;
 
 namespace Arthas.Demo
 {
@@ -9,38 +8,6 @@ namespace Arthas.Demo
         {
             get => GetValue(() => "Arthas.Demo");
             set => SetValue(value);
-        }
-
-        public bool BtnEnabled
-        {
-            get => GetValue(() => true);
-            set => SetValue(value);
-        }
-
-        public AsyncCommand<object> CmdSample
-        {
-            get => GetAsyncCommand<object>(async obj => await Task.Run(async () =>
-            {
-                Title = "Busy...";
-                BtnEnabled = false;
-                //do something
-                await Task.Delay(2000);
-                Title = "Arthas.Demo";
-                BtnEnabled = true;
-            }));
-        }
-
-        public AsyncCommand<string> CmdSampleWithParam
-        {
-            get => GetAsyncCommand<string>(async str => await Task.Run(async () =>
-            {
-                Title = $"Hello I'm {str} currently";
-                BtnEnabled = false;
-                //do something
-                await Task.Delay(2000);
-                Title = "Arthas.Demo";
-                BtnEnabled = true;
-            }));
         }
     }
 }
