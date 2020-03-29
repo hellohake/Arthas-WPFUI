78500490c0470e7ca71a0ae85021b28ca065a616
ONEO <oneo.me@outlook.com>
2020-03-29 22:57:49 +0800

    添加 Card

---
+++ Arthas/Controls/MetroCard.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Arthas.Controls
+{
+    public class MetroCard : GroupBox
+    {
+        static MetroCard()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MetroCard), new FrameworkPropertyMetadata(typeof(MetroCard)));
+        }
+    }
+}
